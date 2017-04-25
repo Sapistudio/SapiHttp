11ae7ebdd9c3454a8462f6f62791e57eb221bd5e
laur sandu <Sapistudio@users.noreply.github.com>
2017-04-25 23:36:02 +0300

    Update Client.php

--- Client.php
+++ Client.php
@@ -3,7 +3,65 @@
 namespace SapiStudio\SapiBrowser;
 use Goutte\Client as BrowserClient;
 
+/**
+ * Client
+ * 
+ * @package 
+ * @author SapiStudio
+ * @copyright 2017
+ * @version $Id$
+ * @access public
+ */
 class Client extends BrowserClient
 {
-   
+    /**
+     * Client::setUserAgent()
+     * 
+     * @param string $user
+     * @return
+     */
+    public function setUserAgent($user=''){
+        return $this->setServerParameter('HTTP_USER_AGENT',$user);
+    }
+    
+    /**
+     * Client::getCookies()
+     * 
+     * @return
+     */
+    public function getCookies(){
+        return $this->getCookieJar()->all();
+    }
+    
+    /**
+     * Client::setCookies()
+     * 
+     * @param mixed $cookies
+     * @return
+     */
+    public function setCookies($cookies){
+        if($cookies){
+            foreach($cookies as $a=>$cookie){
+                $this->getCookieJar()->set($cookie);
+            }
+        }
+        return $this;
+    }
+    
+    /**
+     * Client::hasCookie()
+     * 
+     * @param mixed $cookieName
+     * @return
+     */
+    public function hasCookie($cookieName=null){
+        $cookies = $this->getCookieJar()->all();
+        if($cookies){
+            foreach ($cookies as $a=>$cookie)
+                if ($cookie->getName() == $cookieName){
+                    return true;
+            }
+        }
+        return false;
+    }
 }
