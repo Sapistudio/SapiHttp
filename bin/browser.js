3221f9da71fd14f8676fd7ed1160c34e983010f2
laur sandu <Sapistudio@users.noreply.github.com>
2017-10-26 02:31:15 +0300

    Create browser.js

---
+++ bin/browser.js
@@ -0,0 +1,45 @@
+const puppeteer = require('puppeteer');
+
+const request = JSON.parse(process.argv[2]);
+
+const callChrome = async () => {
+    let browser;
+    let page;
+
+    try {
+        browser = await puppeteer.launch({ args: request.options.args || [] });
+
+        page = await browser.newPage();
+
+        if (request.options && request.options.userAgent) {
+            await page.setUserAgent(request.options.userAgent);
+        }
+
+        if (request.options && request.options.viewport) {
+            await page.setViewport(request.options.viewport);
+        }
+
+        const requestOptions = {};
+
+        if (request.options && request.options.networkIdleTimeout) {
+            requestOptions.waitUntil = 'networkidle';
+            requestOptions.networkIdleTimeout = request.options.networkIdleTimeout;
+        }
+
+        await page.goto(request.url, requestOptions);
+
+        console.log(await page[request.action](request.options));
+
+        await browser.close();
+    } catch (exception) {
+        if (browser) {
+            await browser.close();
+        }
+
+        console.error(exception);
+
+        process.exit(1);
+    }
+};
+
+callChrome();
