ef81e61c2b231b68cb270d146ff061c9f29c7c2f
laur sandu <Sapistudio@users.noreply.github.com>
2017-10-26 02:38:45 +0300

    Update ChromeClient.php

--- src/ChromeClient.php
+++ src/ChromeClient.php
@@ -7,7 +7,7 @@ use SapiStudio\Http\Exceptions\CouldNotTakeBrowsershot;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 
 
-class Browsershot
+class ChromeClient
 {
     protected $nodeBinary           = 'node';
     protected $npmBinary            = 'npm';
