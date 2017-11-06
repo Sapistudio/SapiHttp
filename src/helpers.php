75499369897793e3451ea1df82d4cadcacf0db87
laur sandu <Sapistudio@users.noreply.github.com>
2017-11-07 01:22:44 +0200

    Update helpers.php

--- src/helpers.php
+++ src/helpers.php
@@ -1,1 +1,20 @@
+<?php
 
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Debug\Dumper;
+use Illuminate\Contracts\Support\Htmlable;
+
+if (! function_exists('getDom')) {
+    /**
+     * Assign high numeric IDs to a config item to force appending.
+     *
+     * @param  array  $array
+     * @return array
+     */
+    function getDom(string $string)
+    {
+        return Symfony\Component\DomCrawler\Crawler($string);
+    }
+}
