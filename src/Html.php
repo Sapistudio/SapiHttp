281d54f35c1b43007ba1e0657b0bf45f0ad0f960
laur sandu <Sapistudio@users.noreply.github.com>
2018-04-24 18:11:07 +0300

    Update Html.php

--- src/Html.php
+++ src/Html.php
@@ -26,4 +26,74 @@ class Html
         $this->htmlDom      = $html;
         $this->domCrawler   = getDomCrawler($html);
     }
+    
+    /**
+     * Html::getDomImages()
+     * 
+     * @param mixed $imagesAttributes
+     * @return
+     */
+    public function getDomImages($imagesAttributes = ['src','title','alt']){
+        return $this->domCrawler->filterXpath('//img')->extract($imagesAttributes);
+    }
+    
+    /**
+     * Html::getDomUris()
+     * 
+     * @param mixed $urisAttributes
+     * @return
+     */
+    public function getDomUris($urisAttributes = ['href','title','alt']){
+        return $this->domCrawler->filterXpath('//a')->extract($urisAttributes);
+    }
+    
+    /**
+     * Html::getDomAreas()
+     * 
+     * @param mixed $areasAttributes
+     * @return
+     */
+    public function getDomAreas($areasAttributes = ['href','title','alt']){
+        return $this->domCrawler->filterXpath('//area')->extract($areasAttributes);
+    }
+    
+    /**
+     * Html::getAllLinks()
+     * 
+     * @return
+     */
+    public function getAllLinks(){
+        return array_merge($this->getDomUris(),$this->getDomAreas());
+    }
+    
+    /**
+     * Html::isValidURL()
+     * 
+     * @param mixed $url
+     * @return
+     */
+    public static function isValidURL($url = null){
+        return preg_match('/^(http|https):\\/\\/[a-z0-9_]+([\\-\\.]{1}[a-z_0-9]+)*\\.[_a-z]{2,5}'.'((:[0-9]{1,5})?\\/.*)?$/i' ,$url);
+    }
+    
+    /**
+     * Html::urlIsImage()
+     * 
+     * @param mixed $contentType
+     * @return
+     */
+    public static function urlIsImage($contentType = null){
+        switch ($contentType) {
+            case 'image/png' :
+            case 'image/jpg' :
+            case 'image/jpeg':
+            case 'image/gif' :
+                $urlIsImage = true;
+            break;
+        default:
+                $urlIsImage = false;
+          break;
+      }
+      return $urlIsImage;
+    }
 }
