f3422a8d3776673e097e20b3454819be0ec3b5bf
laur sandu <Sapistudio@users.noreply.github.com>
2020-05-30 00:36:39 +0300

    Update CacheRequest.php

--- src/Browser/CacheRequest.php
+++ src/Browser/CacheRequest.php
@@ -3,28 +3,30 @@ namespace SapiStudio\Http\Browser;
 
 class CacheRequest
 {
-    const CACHE_VALIDITY = 3600;
+    private static  $cacheValidity = 3600;
     private static $tokenName;
     private static $requestClient;
     private static $cacheClient;
     private static $instance;
     
-    /**
-     * CacheRequest::init()
-     * 
-     * @return
-     */
-    public static function init($client = null,$token = null)
+    /** CacheRequest::setCacheValidity() */
+    public static function setCacheValidity($validitySeconds = 1){
+        self::$cacheValidity = $validitySeconds;
+    }
+    
+    /** CacheRequest::init() */
+    public static function init(StreamClient $client,$token = null)
     {
         static::$instance = new static($client,$token);
         return static::$instance;
     }
     
-    /**
-     * CacheRequest::__construct()
-     * 
-     * @return
-     */
+    /** CacheRequest::deleteToken() */
+    public static function deleteToken($tokenName = null){
+        return (self::$cacheClient) ? self::$cacheClient->deleteMultiple([$tokenName]) : false;
+    }
+    
+    /** CacheRequest::__construct()*/
     private function __construct($client = null,$token = null)
     {
         self::$tokenName        = $token;
@@ -32,21 +34,15 @@ class CacheRequest
         self::$cacheClient      = new \Symfony\Component\Cache\Simple\FilesystemCache();
     }
     
-    /**
-     * CacheRequest::__call()
-     * 
-     * @param mixed $name
-     * @param mixed $arguments
-     * @return
-     */
+    /** CacheRequest::__call()*/
     public function __call($name,$arguments)
     {
         $response = self::$cacheClient->get(self::$tokenName);
         if($response)
             return $response;
-        $response = self::$requestClient->$name(...$arguments);
-        $response = self::$requestClient->getBody();
-        self::$cacheClient->set(self::$tokenName,$response,SELF::CACHE_VALIDITY);
+        $response = self::$requestClient->$name(...$arguments)->getBody();
+        if($response)
+            self::$cacheClient->set(self::$tokenName,$response,self::$cacheValidity);
         return $response;
     }
 }
